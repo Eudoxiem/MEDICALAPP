870a26b7c5aa5fff4a7061888dfad8ad7d868186
Eudoxie <mwinkeudoxie@yahoo.fr>
2025-03-05 23:48:27 +0100

    backend et frontend  medicalapp react et springboot

---
+++ src/main/java/com/example/Medicalapp/MedicalappApplication.java
@@ -0,0 +1,13 @@
+package com.example.Medicalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Medicalapp/MedicalappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Medicalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
