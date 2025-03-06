4032f2d5f7c68588b70e77e4f8a01d02ee5a29a1
nix-ones <beanixon@hotmail.fr>
2025-03-06 19:00:35 +0100

    Application properties mise à jour et le packageModel

--- src/main/java/com/example/Model/Roles.java
+++
@@ -1,6 +0,0 @@
-package com.example.Model;
-
-@Entity
-public class Roles {
-
-}
--- src/main/java/com/example/Model/Users.java
+++ src/main/java/com/example/Model/Users.java
@@ -1,7 +1,26 @@
-package com.example.Model;
+package com.example.model;
 
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "Utilisateurs")
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(nullable = false, name = "nom")
+    private String name;
+
+    @Column(nullable = false, name = "Prenom")
+    private String firtName;
+
+    @Column(nullable = false, name = "E-mail", unique = true)
+    private String email;
+
+    @Column(nullable = false, name = "MotDePasse")
+    private String password;
+
+    @Column(nullable = false, name = "DateDeNaissance")
+    private String dateDeNaissance;
 }
